08c606012eaf0357e7cb45a1b428379096613127
welldoer <welldoer@263.net>
2022-05-16 12:57:02 +0000

    绿：提供较少的、必要的功能代码进行测试修复

--- src/main/java/net/blogjava/welldoer/demo/FiberCalculator.java
+++ src/main/java/net/blogjava/welldoer/demo/FiberCalculator.java
@@ -3,6 +3,9 @@ package net.blogjava.welldoer.demo;
 public class FiberCalculator {
 
     public static long calculate(int index) {
+        if (index == 1) {
+            return 1;
+        }
         return 0;
     }
 
