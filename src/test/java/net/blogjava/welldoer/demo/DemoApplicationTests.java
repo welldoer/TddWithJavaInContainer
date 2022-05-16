83a6e4dff0caf47df0f6be8a0b498f1223e8b195
welldoer <welldoer@263.net>
2022-05-16 07:35:57 +0000

    执行 curl.sh 获取 SpringBoot 初始化代码集

---
+++ src/test/java/net/blogjava/welldoer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.blogjava.welldoer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
