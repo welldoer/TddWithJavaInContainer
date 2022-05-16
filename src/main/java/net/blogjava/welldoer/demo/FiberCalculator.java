8a04ef5db585cc571d3d03946a484cefa9bab2c3
welldoer <welldoer@263.net>
2022-05-16 13:27:56 +0000

    绿：快速修改功能代码，通过测试

--- src/main/java/net/blogjava/welldoer/demo/FiberCalculator.java
+++ src/main/java/net/blogjava/welldoer/demo/FiberCalculator.java
@@ -3,7 +3,7 @@ package net.blogjava.welldoer.demo;
 public class FiberCalculator {
 
     public static long calculate(int index) {
-        if (index == 1) {
+        if (index == 1 || index == 2) {
             return 1;
         }
         return 0;
