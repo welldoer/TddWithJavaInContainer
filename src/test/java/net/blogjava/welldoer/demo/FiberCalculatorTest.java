732001d69351128f58d5a6e41a8cc4a300a1b6e4
welldoer <welldoer@263.net>
2022-05-16 13:07:05 +0000

    红：增加新的测试代码，测试失败很正常

--- src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
+++ src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
@@ -8,5 +8,6 @@ public class FiberCalculatorTest {
     @Test
     void should_return_1_given_index_1_or_2() {
         assertThat(FiberCalculator.calculate(1)).isEqualTo(1);
+        assertThat(FiberCalculator.calculate(2)).isEqualTo(1);
     }
 }
