c208bc6f37b8825c147d532539360f2486d91c65
welldoer <welldoer@263.net>
2022-05-16 14:39:52 +0000

    红：增加边界测试代码

--- src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
+++ src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
@@ -3,6 +3,7 @@ package net.blogjava.welldoer.demo;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FiberCalculatorTest {
     @Test
@@ -19,5 +20,10 @@ public class FiberCalculatorTest {
     @Test
     void should_return_12586269025L_given_50() {
         assertThat(FiberCalculator.calculate(50)).isEqualTo(12586269025L);
+    }
+
+    @Test
+    void should_throw_exception_given_index_below_1() {
+        assertThrows(IllegalArgumentException.class, () -> FiberCalculator.calculate(0));
     }
 }
