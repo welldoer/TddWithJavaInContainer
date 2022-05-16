28f2adcfc54df0cf6669fa74c1cac3263d4c20a2
welldoer <welldoer@263.net>
2022-05-16 14:52:15 +0000

    红：增加边界测试代码

--- src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
+++ src/test/java/net/blogjava/welldoer/demo/FiberCalculatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+
 public class FiberCalculatorTest {
     @Test
     void should_return_1_given_index_1_or_2() {
@@ -25,5 +27,11 @@ public class FiberCalculatorTest {
     @Test
     void should_throw_exception_given_index_below_1() {
         assertThrows(IllegalArgumentException.class, () -> FiberCalculator.calculate(0));
+    }
+
+    @Test
+    @DisplayName("throw exception while 50+")
+    void should_throw_exception_given_index_above_50() {
+        assertThrows(IllegalArgumentException.class, () -> FiberCalculator.calculate(51));
     }
 }
